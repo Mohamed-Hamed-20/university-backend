415426d91bd8f1258485746ac5fb3bb6c88037f4
mohamed hamed <125780715+Mohamed-Hamed-20@users.noreply.github.com>
2024-04-17 02:09:05 +0200

    done

--- src/controllers/semsterGrate/semsterGrate.js
+++ src/controllers/semsterGrate/semsterGrate.js
@@ -6,7 +6,7 @@ import { calculateCumulativeGPA, updateGPA } from "../../utils/calcGrates.js";
 import { asyncHandler } from "../../utils/errorHandling.js";
 
 export const addTosemster = asyncHandler(async (req, res, next) => {
-  const { studentId, courseId, semsterId } = req.body;
+  const { studentId, courseId } = req.body;
   const register = req.register;
   const grade = req.Grade;
   const semsterId = req.semsterId;
