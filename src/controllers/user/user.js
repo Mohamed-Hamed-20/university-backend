c7b2974e4de8a7167a604e33c3423d76c30e08bc
mohamed hamed <125780715+Mohamed-Hamed-20@users.noreply.github.com>
2024-05-31 14:47:20 +0300

    safe new updated

--- src/controllers/user/user.js
+++ src/controllers/user/user.js
@@ -79,23 +79,23 @@ export const login = asyncHandler(async (req, res, next) => {
     refreshTokenPromise,
   ]);
 
-  // // encrpt accesss tokens
-  // const encrptAcessTokenpromise = encryptData({
-  //   data: accessToken,
-  //   password: process.env.ACCESS_TOKEN_ENCRPTION,
-  // });
+  // encrpt accesss tokens
+  const encrptAcessTokenpromise = encryptData({
+    data: accessToken,
+    password: process.env.ACCESS_TOKEN_ENCRPTION,
+  });
 
-  // // encrpt refresh tokens
-  // const encrptRefTokenpromise = encryptData({
-  //   data: refreshToken,
-  //   password: process.env.REFRESH_TOKEN_ENCRPTION,
-  // });
+  // encrpt refresh tokens
+  const encrptRefTokenpromise = encryptData({
+    data: refreshToken,
+    password: process.env.REFRESH_TOKEN_ENCRPTION,
+  });
 
   const successpromise = storeRefreshToken(refreshToken, user._id, next);
 
-  const [/*encrptAcessToken, encrptRefToken,*/ success] = await Promise.all([
-    // encrptAcessTokenpromise,
-    // encrptRefTokenpromise,
+  const [encrptAcessToken, encrptRefToken, success] = await Promise.all([
+    encrptAcessTokenpromise,
+    encrptRefTokenpromise,
     successpromise,
   ]);
 
@@ -105,8 +105,8 @@ export const login = asyncHandler(async (req, res, next) => {
 
   return res.status(200).json({
     message: "done login",
-    accessToken: accessToken,
-    refreshToken: refreshToken,
+    accessToken: encrptAcessToken,
+    refreshToken: encrptRefToken,
     role: user.role,
   });
 });
